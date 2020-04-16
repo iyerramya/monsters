2d28fd0a3bcfbc7c735a9434345052055d3293a2
iyerramya <43103509+iyerramya@users.noreply.github.com>
2020-04-16 12:05:07 -0700

    added background and heading

--- src/App.js
+++ src/App.js
@@ -22,13 +22,14 @@ class App extends Component {
   handleChange = (e) => {
     this.setState({ searchField: e.target.value });
   }
-  
+
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter(monster => 
       monster.name.toLowerCase().includes(searchField.toLowerCase()))
     return (
       <div className='App'>
+        <h1>Monsters Rolodex</h1>
         <SearchBox 
           placeholder='Search Monsters' 
           handleChange={this.handleChange}
