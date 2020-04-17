1841902a1157bf1c287d8351279eae3752967025
iyerramya <43103509+iyerramya@users.noreply.github.com>
2020-04-16 19:48:13 -0700

    Updates

---
+++ precache-manifest.75b9003c0646bfd451d42a28475a3a8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58de0a676d66b28e5e62cdd9642f41bc",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "d027c0f655957fd02c1b",
+    "url": "/monsters/static/css/main.25c571db.chunk.css"
+  },
+  {
+    "revision": "2b7d9edd948003b96165",
+    "url": "/monsters/static/js/2.b6796cae.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters/static/js/2.b6796cae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d027c0f655957fd02c1b",
+    "url": "/monsters/static/js/main.f5d47691.chunk.js"
+  },
+  {
+    "revision": "69ad86de3903771917ee",
+    "url": "/monsters/static/js/runtime-main.5e6993dc.js"
+  }
+]);
